195d530c4920cc16de0b7d03a21eedb7086abbaf
Вадим Бугаков <vbugakov55@gmail.com>
2019-03-17 13:23:24 +0300

    changes

--- DiningEasy/DiningEasy/MenuPage.xaml.cs
+++ DiningEasy/DiningEasy/MenuPage.xaml.cs
@@ -9,9 +9,13 @@ namespace DiningEasy
 {
 	public partial class MenuPage : ContentPage
 	{
-		public MenuPage()
+        public string[] Phones { get; set; }
+
+        public MenuPage()
 		{
-			InitializeComponent();
-		}
+            InitializeComponent();
+            Phones = new string[] { "iPhone 8", "Samsung Galaxy S9", "Huawei P10", "LG G6" };
+            this.BindingContext = this;
+        }
 	}
 }
