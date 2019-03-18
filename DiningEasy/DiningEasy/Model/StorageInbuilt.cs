45c1c07a6f8e0c11a9596112ec11bb4f265ac8ab
Даниил <pleshkov631@gmail.com>
2019-03-18 18:59:22 +0300

    new

--- DiningEasy/DiningEasy/Model/StorageInbuilt.cs
+++ DiningEasy/DiningEasy/Model/StorageInbuilt.cs
@@ -24,7 +24,7 @@ namespace DiningEasy.Model
                                 Description = "Вкусный борщ со сметаной в лучших традициях русской кухни.",
                                 Ingridients = "Вода, свекла, картошка, мясо, зелень, сметана",
                                 Weight = 300,
-                                Cost = 150 
+                                Cost = 150
                             },
                             new Product
                             {
@@ -32,7 +32,7 @@ namespace DiningEasy.Model
                                 Description = "Постные щи идеально подойдут для тех, кто следит за своей фигурой.",
                                 Ingridients = "Вода, капуста, картошка, лук, залень",
                                 Weight = 300,
-                                Cost = 130 
+                                Cost = 130
                             },
                             new Product
                             {
@@ -40,11 +40,11 @@ namespace DiningEasy.Model
                                 Description = "Захотелось чего то новенького? Попробуйте фирменной ухи от шеф-повара!",
                                 Ingridients = "Вода, мясо",
                                 Weight = 200,
-                                Cost = 180 
+                                Cost = 180
                             }
                         }
                     },
-                    new Category 
+                    new Category
                     {
                         Name = "Салаты",
                         Products = new[]
@@ -55,7 +55,7 @@ namespace DiningEasy.Model
                                 Description = "Низкокаллорийный салат с индейкой специально для тех, кто следит за своей фигурой.",
                                 Ingridients = "Огурцы, салат, помидоры, сыр, майонез",
                                 Weight = 200,
-                                Cost = 100 
+                                Cost = 100
                             },
                             new Product
                             {
@@ -63,7 +63,7 @@ namespace DiningEasy.Model
                                 Description = "Вкусный, легкий салат для истиных гурманов.",
                                 Ingridients = "Салат, сыр, сметана, чеснок, майонез",
                                 Weight = 200,
-                                Cost = 120 
+                                Cost = 120
                             },
                             new Product
                             {
@@ -71,19 +71,21 @@ namespace DiningEasy.Model
                                 Description = "Хотите попробовать что то из национальных русских блюд? Пожалуйста)",
                                 Ingridients = "Яйцо, картошка, сыр, колбаса, майонез",
                                 Weight = 150,
-                                Cost = 90 
+                                Cost = 90
                             },
                         }
                     },
                     new Category {
                         Name = "Десерты" ,
+                        Products = new[]
+                        {
                         new Product
                             {
                                 Name = "Печенье",
                                 Description = "Вкусное печенье с шоколадной крошкой для тех, кто любит побаловать себя сладким.",
                                 Ingridients = "Мука, вода, масло, какао",
                                 Weight = 100,
-                                Cost = 80 
+                                Cost = 80
                             },
                         new Product
                             {
@@ -91,8 +93,9 @@ namespace DiningEasy.Model
                                 Description = "Подойдет настоящим гурманам, подается с мороженым)",
                                 Ingridients = "Мука, вода, масло, яблки, сахар, яйца",
                                 Weight = 200,
-                                Cost = 200 
+                                Cost = 200
                             },
+                        }
                     }
                 }
             },
@@ -112,7 +115,7 @@ namespace DiningEasy.Model
                                 Description = "Вкусный борщ со сметаной в лучших традициях русской кухни.",
                                 Ingridients = "Вода, свекла, картошка, мясо, зелень, сметана",
                                 Weight = 300,
-                                Cost = 150 
+                                Cost = 150
                             },
                             new Product
                             {
@@ -120,11 +123,11 @@ namespace DiningEasy.Model
                                 Description = "Постные щи идеально подойдут для тех, кто следит за своей фигурой.",
                                 Ingridients = "Вода, капуста, картошка, лук, залень",
                                 Weight = 300,
-                                Cost = 130 
+                                Cost = 130
                             }
                         }
                     },
-                    new Category 
+                    new Category
                     {
                         Name = "Салаты",
                         Products = new[]
@@ -135,7 +138,7 @@ namespace DiningEasy.Model
                                 Description = "Низкокаллорийный салат с индейкой специально для тех, кто следит за своей фигурой.",
                                 Ingridients = "Огурцы, салат, помидоры, сыр, майонез",
                                 Weight = 200,
-                                Cost = 100 
+                                Cost = 100
                             },
                             new Product
                             {
@@ -143,7 +146,7 @@ namespace DiningEasy.Model
                                 Description = "Вкусный, легкий салат для истиных гурманов.",
                                 Ingridients = "Салат, сыр, сметана, чеснок, майонез",
                                 Weight = 200,
-                                Cost = 120 
+                                Cost = 120
                             },
                             new Product
                             {
@@ -151,19 +154,21 @@ namespace DiningEasy.Model
                                 Description = "Хотите попробовать что то из национальных русских блюд? Пожалуйста)",
                                 Ingridients = "Яйцо, картошка, сыр, колбаса, майонез",
                                 Weight = 150,
-                                Cost = 90 
+                                Cost = 90
                             },
                         }
                     },
                     new Category {
                         Name = "Напитки" ,
+                        Products = new[]
+                        {
                         new Product
                             {
                                 Name = "Чай",
                                 Description = "Хотите согреться? Наш ароматнейший чай поможет вам в этом)",
                                 Ingridients = "Листовой чай, вода",
                                 Weight = 200,
-                                Cost = 30 
+                                Cost = 30
                             },
                         new Product
                             {
@@ -171,8 +176,9 @@ namespace DiningEasy.Model
                                 Description = "Поможет вам взбодриться сонным пасмурным утром и не только)",
                                 Ingridients = "Кофейный зерна, вода",
                                 Weight = 200,
-                                Cost = 100 
+                                Cost = 100
                             },
+                        }
                     }
                 }
             },
@@ -192,7 +198,7 @@ namespace DiningEasy.Model
                                 Description = "Вкусный борщ со сметаной в лучших традициях русской кухни.",
                                 Ingridients = "Вода, свекла, картошка, мясо, зелень, сметана",
                                 Weight = 300,
-                                Cost = 150 
+                                Cost = 150
                             },
                             new Product
                             {
@@ -200,11 +206,11 @@ namespace DiningEasy.Model
                                 Description = "Захотелось чего то новенького? Попробуйте фирменной ухи от шеф-повара!",
                                 Ingridients = "Вода, мясо",
                                 Weight = 200,
-                                Cost = 180 
+                                Cost = 180
                             }
                         }
                     },
-                    new Category 
+                    new Category
                     {
                         Name = "Салаты",
                         Products = new[]
@@ -215,7 +221,7 @@ namespace DiningEasy.Model
                                 Description = "Низкокаллорийный салат с индейкой специально для тех, кто следит за своей фигурой.",
                                 Ingridients = "Огурцы, салат, помидоры, сыр, майонез",
                                 Weight = 200,
-                                Cost = 100 
+                                Cost = 100
                             },
                             new Product
                             {
@@ -223,19 +229,21 @@ namespace DiningEasy.Model
                                 Description = "Хотите попробовать что то из национальных русских блюд? Пожалуйста)",
                                 Ingridients = "Яйцо, картошка, сыр, колбаса, майонез",
                                 Weight = 150,
-                                Cost = 90 
+                                Cost = 90
                             },
                         }
                     },
                     new Category {
                         Name = "Десерты" ,
+                        Products = new[]
+                        {
                         new Product
                             {
                                 Name = "Печенье",
                                 Description = "Вкусное печенье с шоколадной крошкой для тех, кто любит побаловать себя сладким.",
                                 Ingridients = "Мука, вода, масло, какао",
                                 Weight = 100,
-                                Cost = 80 
+                                Cost = 80
                             },
                         new Product
                             {
@@ -243,8 +251,9 @@ namespace DiningEasy.Model
                                 Description = "Подойдет настоящим гурманам, подается с мороженым)",
                                 Ingridients = "Мука, вода, масло, яблки, сахар, яйца",
                                 Weight = 200,
-                                Cost = 200 
+                                Cost = 200
                             },
+                        }
                     }
                 }
             },
