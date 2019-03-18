f7ebb07874415dba8e366bdc4e6487bfeb0cae4f
Даниил <pleshkov631@gmail.com>
2019-03-18 21:00:00 +0300

    new path to images

---
+++ DiningEasy/DiningEasy/CategoryPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+namespace DiningEasy
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CategoryPage : ContentView
+	{
+        ObservableCollection<IProduct> products = new ObservableCollection<IProduct>();
+
+        public ObservableCollection<IProduct> Products
+        {
+            get
+            {
+                return products;
+            }
+            set
+            {
+                if (products != value)
+                {
+                    products = value;
+                    OnPropertyChanged(nameof(Products));
+                }
+            }
+        }
+        public CategoryPage ()
+		{
+            InitializeComponent();
+            BindingContext = this;
+
+            Load();
+        }
+        void Load()
+        {
+            foreach (var prodact in Model.Manager.CurrentCanteen.Categories)
+                Products.Add(prodact);
+        }
+    }
+}
