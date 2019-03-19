382635f739738138d77f622828bec911555dbebf
Даниил <pleshkov631@gmail.com>
2019-03-19 18:49:10 +0300

    k

--- DiningEasy/DiningEasy/Model/StorageInbuilt.cs
+++ DiningEasy/DiningEasy/Model/StorageInbuilt.cs
@@ -21,6 +21,7 @@ namespace DiningEasy.Model
                         {
                             new Product
                             {
+                                Path="Borsh.jpg",
                                 Name = "Борщ",
                                 Description = "Вкусный борщ со сметаной в лучших традициях русской кухни.",
                                 Ingridients = "Вода, свекла, картошка, мясо, зелень, сметана",
@@ -29,6 +30,7 @@ namespace DiningEasy.Model
                             },
                             new Product
                             {
+                                Path="Shi.jpg",
                                 Name = "Щи",
                                 Description = "Постные щи идеально подойдут для тех, кто следит за своей фигурой.",
                                 Ingridients = "Вода, капуста, картошка, лук, залень",
@@ -37,6 +39,7 @@ namespace DiningEasy.Model
                             },
                             new Product
                             {
+                                Path="Uha.jpg",
                                 Name = "Уха",
                                 Description = "Захотелось чего то новенького? Попробуйте фирменной ухи от шеф-повара!",
                                 Ingridients = "Вода, мясо",
@@ -53,6 +56,7 @@ namespace DiningEasy.Model
                         {
                             new Product
                             {
+                                Path="Grecheskij.jpg",
                                 Name = "Греческий",
                                 Description = "Низкокаллорийный салат с индейкой специально для тех, кто следит за своей фигурой.",
                                 Ingridients = "Огурцы, салат, помидоры, сыр, майонез",
@@ -61,6 +65,7 @@ namespace DiningEasy.Model
                             },
                             new Product
                             {
+                                Path="Cezar.jpg",
                                 Name = "Цезарь",
                                 Description = "Вкусный, легкий салат для истиных гурманов.",
                                 Ingridients = "Салат, сыр, сметана, чеснок, майонез",
@@ -69,6 +74,7 @@ namespace DiningEasy.Model
                             },
                             new Product
                             {
+                                Path="Olivie.jpg",
                                 Name = "Оливье",
                                 Description = "Хотите попробовать что то из национальных русских блюд? Пожалуйста)",
                                 Ingridients = "Яйцо, картошка, сыр, колбаса, майонез",
@@ -84,6 +90,7 @@ namespace DiningEasy.Model
                         {
                         new Product
                             {
+                                Path="Cookie.jpg",
                                 Name = "Печенье",
                                 Description = "Вкусное печенье с шоколадной крошкой для тех, кто любит побаловать себя сладким.",
                                 Ingridients = "Мука, вода, масло, какао",
@@ -92,6 +99,7 @@ namespace DiningEasy.Model
                             },
                         new Product
                             {
+                                Path="Shtrudel.jpg",
                                 Name = "Яблочный штрудель",
                                 Description = "Подойдет настоящим гурманам, подается с мороженым)",
                                 Ingridients = "Мука, вода, масло, яблки, сахар, яйца",
@@ -109,11 +117,13 @@ namespace DiningEasy.Model
                 {
                     new Category
                     {
+                        Path="Sup.jpg",
                         Name = "Супы",
                         Products = new[]
                         {
                             new Product
                             {
+                                Path="Borsh.jpg",
                                 Name = "Борщ",
                                 Description = "Вкусный борщ со сметаной в лучших традициях русской кухни.",
                                 Ingridients = "Вода, свекла, картошка, мясо, зелень, сметана",
@@ -122,6 +132,7 @@ namespace DiningEasy.Model
                             },
                             new Product
                             {
+                                Path="Shi.jpg",
                                 Name = "Щи",
                                 Description = "Постные щи идеально подойдут для тех, кто следит за своей фигурой.",
                                 Ingridients = "Вода, капуста, картошка, лук, залень",
@@ -132,11 +143,13 @@ namespace DiningEasy.Model
                     },
                     new Category
                     {
+                        Path="Salad.jpg",
                         Name = "Салаты",
                         Products = new[]
                         {
                             new Product
                             {
+                                Path="Grecheskij.jpg",
                                 Name = "Греческий",
                                 Description = "Низкокаллорийный салат с индейкой специально для тех, кто следит за своей фигурой.",
                                 Ingridients = "Огурцы, салат, помидоры, сыр, майонез",
@@ -145,6 +158,7 @@ namespace DiningEasy.Model
                             },
                             new Product
                             {
+                                Path="Cezar.jpg",
                                 Name = "Цезарь",
                                 Description = "Вкусный, легкий салат для истиных гурманов.",
                                 Ingridients = "Салат, сыр, сметана, чеснок, майонез",
@@ -153,6 +167,7 @@ namespace DiningEasy.Model
                             },
                             new Product
                             {
+                                Path="Olivie.jpg",
                                 Name = "Оливье",
                                 Description = "Хотите попробовать что то из национальных русских блюд? Пожалуйста)",
                                 Ingridients = "Яйцо, картошка, сыр, колбаса, майонез",
@@ -162,11 +177,13 @@ namespace DiningEasy.Model
                         }
                     },
                     new Category {
+                        Path="Beverages.jpg",
                         Name = "Напитки" ,
                         Products = new[]
                         {
                         new Product
                             {
+                                Path="Beverages.jpg",
                                 Name = "Чай",
                                 Description = "Хотите согреться? Наш ароматнейший чай поможет вам в этом)",
                                 Ingridients = "Листовой чай, вода",
@@ -175,6 +192,7 @@ namespace DiningEasy.Model
                             },
                         new Product
                             {
+                                Path="Coffee.jpg",
                                 Name = "Кофе",
                                 Description = "Поможет вам взбодриться сонным пасмурным утром и не только)",
                                 Ingridients = "Кофейный зерна, вода",
@@ -192,11 +210,13 @@ namespace DiningEasy.Model
                 {
                     new Category
                     {
+                        Path="Sup.jpg",
                         Name = "Супы",
                         Products = new[]
                         {
                             new Product
                             {
+                                Path="Borsh.jpg",
                                 Name = "Борщ",
                                 Description = "Вкусный борщ со сметаной в лучших традициях русской кухни.",
                                 Ingridients = "Вода, свекла, картошка, мясо, зелень, сметана",
@@ -205,6 +225,7 @@ namespace DiningEasy.Model
                             },
                             new Product
                             {
+                                Path="Uha.jpg",
                                 Name = "Уха",
                                 Description = "Захотелось чего то новенького? Попробуйте фирменной ухи от шеф-повара!",
                                 Ingridients = "Вода, мясо",
@@ -215,11 +236,13 @@ namespace DiningEasy.Model
                     },
                     new Category
                     {
+                        Path="Salad.jpg",
                         Name = "Салаты",
                         Products = new[]
                         {
                             new Product
                             {
+                                Path="Grecheskij.jpg",
                                 Name = "Греческий",
                                 Description = "Низкокаллорийный салат с индейкой специально для тех, кто следит за своей фигурой.",
                                 Ingridients = "Огурцы, салат, помидоры, сыр, майонез",
@@ -228,6 +251,7 @@ namespace DiningEasy.Model
                             },
                             new Product
                             {
+                                Path="Olivie.jpg",
                                 Name = "Оливье",
                                 Description = "Хотите попробовать что то из национальных русских блюд? Пожалуйста)",
                                 Ingridients = "Яйцо, картошка, сыр, колбаса, майонез",
@@ -237,6 +261,7 @@ namespace DiningEasy.Model
                         }
                     },
                     new Category {
+                        Path="Desert.jpg",
                         Name = "Десерты" ,
                         Products = new[]
                         {
@@ -251,6 +276,7 @@ namespace DiningEasy.Model
                             },
                         new Product
                             {
+                                Path="Shtrudel.jpg",
                                 Name = "Яблочный штрудель",
                                 Description = "Подойдет настоящим гурманам, подается с мороженым)",
                                 Ingridients = "Мука, вода, масло, яблки, сахар, яйца",
